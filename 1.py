1b2363e7e2b9ef2df5a89bf2edb6c0108724abe3
byungsu <yoo3077@gmail.com>
2021-07-01 14:16:53 +0900

    .

--- 1.py
+++ 1.py
@@ -1,22 +1,10 @@
-#!/usr/bin/env python
-import os
-import sys
+G = int(input())
+even_numbers = [n for n in list(range(1, G + 1)) if n % 2 == 0]
+count = 0
+for n in even_numbers:
+    while n:
+        if (n % 10) in [6, 8, 9]:
+            count += 1
+        n = n // 10
 
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Graduate.settings.dev")
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError:
-        # The above import may fail for some other reason. Ensure that the
-        # issue is really that Django is missing to avoid masking other
-        # exceptions on Python 2.
-        try:
-            import django
-        except ImportError:
-            raise ImportError(
-                "Couldn't import Django. Are you sure it's installed and "
-                "available on your PYTHONPATH environment variable? Did you "
-                "forget to activate a virtual environment?"
-            )
-        raise
-    execute_from_command_line(sys.argv)
+print(count)
